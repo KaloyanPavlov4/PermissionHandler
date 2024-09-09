5c2facc8c8f3465350cd873bbab7e4774f2718fd
Kaloyan Pavlov <kaloyanpavlov4@gmail.com>
2024-09-09 17:36:39 +0300

    Implement PermissionHandler

--- src/PermissionHandler.java
+++ src/PermissionHandler.java
@@ -10,19 +10,44 @@ public class PermissionHandler {
     }
 
     public void handleCommand(String command){
+        String[] commandParts = command.split(":");
 
+        //Deny
+        if (commandParts.length == 3){
+            denyPermission(commandParts[0],commandParts[1].toUpperCase());
+        }
+
+        //Allow
+        else {
+            if (commandParts[0].equals("*") && commandParts[1].equals("*")) clearPermissions();
+            else allowPermission(commandParts[0], commandParts[1].toUpperCase());
+        }
     }
 
-    public void allowPermission(String resource, Operation operation){
-        deniedOperations.get(resource).add(operation);
-    }
-
-    public void denyPermission(String resource, Operation operation){
-        if (deniedOperations.containsKey(resource)) {
-            deniedOperations.get(resource).add(operation);
+    public void allowPermission(String resource, String operation){
+        if (operation.equals("*")){
+            deniedOperations.get(resource).clear();
             return;
         }
-        deniedOperations.put(resource, EnumSet.of(operation));
+
+        Operation op = Operation.valueOf(operation);
+        deniedOperations.get(resource).remove(op);
+    }
+
+    public void denyPermission(String resource, String operation){
+        EnumSet<Operation> toAdd = EnumSet.noneOf(Operation.class);
+        if (operation.equals("*")){
+            toAdd.addAll(EnumSet.allOf(Operation.class));
+        } else {
+            toAdd.add(Operation.valueOf(operation));
+        }
+
+        if (deniedOperations.containsKey(resource)) {
+            deniedOperations.get(resource).addAll(toAdd);
+            return;
+        }
+
+        deniedOperations.put(resource, toAdd);
     }
 
     public void clearPermissions() {
@@ -31,10 +56,17 @@ public class PermissionHandler {
     }
 
     public boolean isAllowed(String resource, Operation operation){
-
+        boolean isResourceAllowed = !deniedOperations.get(resource).contains(operation);
+        boolean areAllResourcesAllowed = !deniedOperations.get("*").contains(operation);
+        return isResourceAllowed && areAllResourcesAllowed;
     }
 
     public String allowedPermissions(String resource){
-
+        EnumSet<Operation> allowed = EnumSet.allOf(Operation.class);
+        EnumSet<Operation> denied = EnumSet.noneOf(Operation.class);
+        denied.addAll(deniedOperations.get("*"));
+        denied.addAll(deniedOperations.get(resource));
+        allowed.removeAll(denied);
+        return String.format("%s resource is allowed to do the following operations: %s",resource,allowed);
     }
 }
