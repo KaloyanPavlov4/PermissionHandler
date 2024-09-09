e87e16cf6bbb50ccee32c9bdae9023dd7c93421b
Kaloyan Pavlov <kaloyanpavlov4@gmail.com>
2024-09-09 18:10:13 +0300

    Implement Main and fix edge cases in PermissionHandler

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+            PermissionHandler permissionHandler = new PermissionHandler();
+            List<String> commands = new LinkedList<>();
+            Scanner scanner = new Scanner(System.in);
 
+            String command = scanner.nextLine();
+            while (!command.equalsIgnoreCase("END")){
+                commands.add(command);
+                command = scanner.nextLine();
+            }
+
+            for (String c : commands){
+                permissionHandler.handleCommand(c);
+            }
+
+            while (true) {
+                System.out.println("1.See allowed operations\n2.Check permission on operation");
+                String input = scanner.nextLine();
+                if (input.equals("1")){
+                    System.out.print("Resource: ");
+                    String resource = scanner.nextLine();
+                    System.out.println(permissionHandler.allowedPermissions(resource));
+                }else if (input.equals("2")){
+                    System.out.print("Resource: ");
+                    String resource = scanner.nextLine();
+                    System.out.print("Operation: ");
+                    String operation = scanner.nextLine();
+                    System.out.println(permissionHandler.isAllowed(resource,operation));
+                }
+            }
         }
     }
--- src/PermissionHandler.java
+++ src/PermissionHandler.java
@@ -12,25 +12,39 @@ public class PermissionHandler {
     public void handleCommand(String command){
         String[] commandParts = command.split(":");
 
+        if (commandParts.length > 3 || commandParts.length < 2){
+            throw new IllegalArgumentException("Invalid command length!");
+        }
+
         //Deny
         if (commandParts.length == 3){
-            denyPermission(commandParts[0],commandParts[1].toUpperCase());
+            if (!commandParts[2].equalsIgnoreCase("deny")) throw new IllegalArgumentException("Invalid third part of command!");
+            denyPermission(commandParts[0],commandParts[1]);
         }
 
         //Allow
         else {
-            if (commandParts[0].equals("*") && commandParts[1].equals("*")) clearPermissions();
-            else allowPermission(commandParts[0], commandParts[1].toUpperCase());
+            allowPermission(commandParts[0], commandParts[1]);
         }
     }
 
     private void allowPermission(String resource, String operation){
+        if (resource.equals("*")){
+            if (operation.equals("*")) {
+                clearPermissions();
+                return;
+            }
+            Operation op = Operation.valueOf(operation.toUpperCase());
+            deniedOperations.forEach((key, set) -> set.remove(op));
+            return;
+        }
+
         if (operation.equals("*")){
             deniedOperations.get(resource).clear();
             return;
         }
 
-        Operation op = Operation.valueOf(operation);
+        Operation op = Operation.valueOf(operation.toUpperCase());
         deniedOperations.get(resource).remove(op);
     }
 
@@ -39,7 +53,7 @@ public class PermissionHandler {
         if (operation.equals("*")){
             toAdd.addAll(EnumSet.allOf(Operation.class));
         } else {
-            toAdd.add(Operation.valueOf(operation));
+            toAdd.add(Operation.valueOf(operation.toUpperCase()));
         }
 
         if (deniedOperations.containsKey(resource)) {
@@ -56,7 +70,7 @@ public class PermissionHandler {
     }
 
     public boolean isAllowed(String resource, String operation){
-        Operation op = Operation.valueOf(operation);
+        Operation op = Operation.valueOf(operation.toUpperCase());
         boolean isResourceAllowed = !deniedOperations.containsKey(resource) || !deniedOperations.get(resource).contains(op);
         boolean areAllResourcesAllowed = !deniedOperations.get("*").contains(op);
         return isResourceAllowed && areAllResourcesAllowed;
@@ -65,6 +79,7 @@ public class PermissionHandler {
     public String allowedPermissions(String resource){
         EnumSet<Operation> allowed = EnumSet.allOf(Operation.class);
         EnumSet<Operation> denied = EnumSet.noneOf(Operation.class);
+
         denied.addAll(deniedOperations.get("*"));
         if (deniedOperations.containsKey(resource)){
             denied.addAll(deniedOperations.get(resource));
